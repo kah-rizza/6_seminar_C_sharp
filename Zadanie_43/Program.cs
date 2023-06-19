79f623faf4f1b0fad81aa4f0cfd523b84fe57a45
kah-rizza <bu95509@gmail.com>
2023-06-20 00:49:57 +0600

    точка пересечения двух прямых

--- Zadanie_43/Program.cs
+++ Zadanie_43/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения: ({x}; {y})");
+    }
+}
